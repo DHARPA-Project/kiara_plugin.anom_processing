1ec4fd7e6443c621beebe1bd478a996e36208faf
Mariella CC <MariellaCC@users.noreply.github.com>
2022-11-02 22:10:55 +0100

    add location info presence check

--- src/kiara_plugin/anom_processing/modules/initial_preprocess.py
+++ src/kiara_plugin/anom_processing/modules/initial_preprocess.py
@@ -1,6 +1,7 @@
 from kiara import KiaraModule, KiaraModuleConfig, ValueMap, ValueMapSchema
 from pydantic import Field
 import pyarrow as pa
+import ast
 import re
 
 
@@ -109,3 +110,132 @@ class ColumnNameReplace(KiaraModule):
         table_pa = pa.Table.from_pandas(df)
         
         outputs.set_value("table", table_pa)
+
+class RemoveNans(KiaraModule):
+    """Remove rows containing "not a number" values for a given column.
+
+    """
+
+    _module_type_name = "anom_processing.remove_nans"
+
+    def create_inputs_schema(
+        self,
+    ) -> ValueMapSchema:
+
+
+        inputs = {
+            "table": {"type": "table", "doc": "The table for which all the rows containing nans need to be removed for a given column."},
+            "column": {"type": "string", "doc": "The column where nans need to be removed."},
+        }
+
+        return inputs
+
+    def create_outputs_schema(
+        self,
+    ) -> ValueMapSchema:
+
+        outputs = {
+            "table": {
+                "type": "table",
+                "doc": "The table without nans for a given column.",
+            }
+        }
+        return outputs
+
+    def process(self, inputs: ValueMap, outputs: ValueMap) -> None:
+
+        table_obj = inputs.get_value_obj("table")
+        col = inputs.get_value_obj("column").data
+
+        df = table_obj.data.to_pandas()
+
+        # this operation should probably also be done with the sql module instead
+
+        df = df[df[col].notna()]
+
+        table_pa = pa.Table.from_pandas(df)
+        
+        outputs.set_value("table", table_pa)
+
+
+class CoordsCheck(KiaraModule):
+    """
+    This module aims at comparing two tables: one table that provides observations with a place name, and one table that 
+    includes latitudes, longitudes and place names.
+    A verification is performed to see if all place names of the first dataset are included in the second dataset.
+    In this specific example, the first table includes several place names per row.
+    At the moment the module only covers the case of the specific example dataset (several locations per row),
+    this should be improved in the future.
+    """
+
+    _module_type_name = "anom_processing.coords_check"
+
+    def create_inputs_schema(
+        self,
+    ) -> ValueMapSchema:
+
+
+        inputs = {
+            "table1": {"type": "table", "doc": "The table that contains the observations."},
+            "table2": {"type": "table", "doc": "The table that contains latitude and longitude information."},
+            "column1": {"type": "string", "doc": "The column that contains the place names in table 1."},
+            "column2": {"type": "string", "doc": "The column that contains the place names in table 2."},
+            "sample_nr": {"type": "integer", "doc": "Number of observations to include. As this operation is very intensive it is advised to use a sample."},
+        }
+
+        return inputs
+
+    def create_outputs_schema(
+        self,
+    ) -> ValueMapSchema:
+
+        outputs = {
+            "result": {
+                "type": "string",
+                "doc": "Information about the result (all coords present or missing coords).",
+            },
+            "places_list": {
+                "type": "list",
+                "doc": "List of the place names that are not included.",
+            }
+        }
+        return outputs
+
+    def process(self, inputs: ValueMap, outputs: ValueMap) -> None:
+
+        table_obj1 = inputs.get_value_obj("table1")
+        table_obj2 = inputs.get_value_obj("table2")
+
+        col1 = inputs.get_value_obj("column1").data
+        col2 = inputs.get_value_obj("column2").data
+
+        num_sample = inputs.get_value_obj("sample_nr").data
+
+        df1 = table_obj1.data.to_pandas()
+        df1 = df1.sample(n=num_sample, random_state=1)
+        df2 = table_obj2.data.to_pandas()
+
+
+        col_loc = df1.columns.get_loc(col1)
+        
+        places = []
+
+        for index, row in enumerate(df1.itertuples(index=False)):
+            #this is not very elegant but no time to make it right
+            item = re.sub(r"^'",'"',row[col_loc])
+            item = re.sub(r"'$",'"',item)
+            try:
+                ls = ast.literal_eval(item)
+                for place in ls:
+                    if place not in places:
+                        places.append(place)
+            except:
+                print(f"{item} not evaluated")
+                                
+
+        not_found_ls = [item for item in places if item not in list(df2[col2])]
+
+        res = "All coordinates were found." if len(not_found_ls) == 0 else "Missing coordinates."
+
+        outputs.set_value("result", res)
+        outputs.set_value("places_list", not_found_ls)
